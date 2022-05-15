3a4315615016595e5be8f7e7b6d7491ed6a0d561
Vikas Rushi <88543171+0xVikasRushi@users.noreply.github.com>
2022-05-15 09:45:43 +0000

    hackerrank

---
+++ Conditional_loops/loops_hackerrank.java
@@ -0,0 +1,15 @@
+package Conditional_loops;
+
+import java.util.Scanner;
+
+public class loops_hackerrank {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        for(int i = 1 ; i<=10;i++){
+            System.out.println(N +" x " + i + " = " + i*N);
+        }
+
+    }
+
+}
