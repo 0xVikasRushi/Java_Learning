3ed07fd26d791aa2f95a57c4923ece89c993657a
Vikas Rushi <88543171+0xVikasRushi@users.noreply.github.com>
2022-05-15 18:01:28 +0000

    Add files via upload

---
+++ Functions_And_methods./evenororddfunc.java
@@ -0,0 +1,19 @@
+package Functions_and_methods;
+
+public class evenororddfunc {
+    public static void main(String[] args) {
+        evenorodd(4);
+    }
+
+    static void evenorodd(int a) {
+        if(a%2==0){
+            System.out.println("A is even");
+        }
+        else {
+            System.out.println("A is odd");
+        }
+
+
+    }
+
+}
