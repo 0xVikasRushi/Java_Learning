e4322312598c048322e71e8a96f3c20f62d70378
Vikas Rushi <88543171+0xVikasRushi@users.noreply.github.com>
2022-06-07 00:57:26 +0530

    Kunal Kushwaha youtube

---
+++ recursion/fib_recursion.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class fib_recursion {
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+
+    static int fib(int n) {
+        if (n < 2) {
+            return n;
+         }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
