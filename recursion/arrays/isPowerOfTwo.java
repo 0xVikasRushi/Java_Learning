21b6b70e78f619259d20210f096ffcc5d653ef5a
Vikas Rushi <88543171+0xVikasRushi@users.noreply.github.com>
2022-06-08 01:18:40 +0530

    231. Power of Two

---
+++ recursion/arrays/isPowerOfTwo.java
@@ -0,0 +1,27 @@
+package recursion.arrays;
+
+public class isPowerOfTwo {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+    }
+
+
+    public static boolean isPowerOfTwo(int n, int x) {
+        if(n == Math.pow(2,x)){
+            return true;
+        }
+        return isPowerOfTwo(n,x+1);
+
+    }
+//    public static boolean isPowerOfTwo(int n) {
+//        return helper(n,0);
+//    }
+//
+//    private static boolean helper(int n, int x) {
+//        if(n == 1){
+//            return true;
+//        }
+//        return helper(n,x+1);
+//    }
+
+}
