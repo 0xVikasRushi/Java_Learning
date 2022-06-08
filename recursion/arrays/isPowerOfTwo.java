2fe526a3aadf7a7fb2d06d7f37393c166660f7f6
Vikas Rushi <88543171+0xVikasRushi@users.noreply.github.com>
2022-06-08 21:38:06 +0530

    Update isPowerOfTwo.java

--- recursion/arrays/isPowerOfTwo.java
+++ recursion/arrays/isPowerOfTwo.java
@@ -4,15 +4,26 @@ public class isPowerOfTwo {
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(1));
     }
-
-
-    public static boolean isPowerOfTwo(int n, int x) {
-        if(n == Math.pow(2,x)){
+  public boolean isPowerOfThree(int n) {
+        return helper(n,0);
+    }
+    static boolean helper(int n , int x){
+        if(n<Math.pow(2,x)){
+            return false;
+        }
+        else if( n == Math.pow(2,x)){
             return true;
         }
-        return isPowerOfTwo(n,x+1);
-
+        else return  helper(n,x+1);
     }
+// similar questions like power 2 3 4 can be solved using this approach 
+//     public static boolean isPowerOfTwo(int n, int x) {
+//         if(n == Math.pow(2,x)){
+//             return true;
+//         }
+//         return isPowerOfTwo(n,x+1);
+
+//     }
 //    public static boolean isPowerOfTwo(int n) {
 //        return helper(n,0);
 //    }
